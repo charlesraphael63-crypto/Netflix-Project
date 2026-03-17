5bf98119a2c0be26cc8bd9ccc46ff4eee9594aa0
Raphael Oluwapelumi john <charlesraphael63-crypto@example.com>
2026-03-18 00:01:37 +0100

    I added Other componetn like Hero, Section and DownSection An their css

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./App.css"
+import LandingPage from "./Pages/LandingPage.jsx"
+
+const App = () => {
+  return (
+    <div>
+      <LandingPage />
+    </div>
+  )
+}
+
+export default App
+
